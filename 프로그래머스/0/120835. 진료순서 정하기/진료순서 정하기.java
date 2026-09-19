1009e6cdf3bf989230051b294380b86e1220c9dd
Seung Tae LEE <dltmdxo1589@daum.net>
2026-09-20 03:11:55 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료순서 정하기/진료순서 정하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        int cnt = 1;
+        
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < emergency.length; j++) {
+                if (emergency[i] < emergency[j]) {
+                    cnt++;
+                }
+            }
+            answer[i] = cnt;
+            cnt = 1;
+        }
+        
+        return answer;
+    }
+}
